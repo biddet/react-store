a54f63ec5f6581685a21ca180c21c18642b69a35
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-03-23 16:45:42 +1100

    fetch data from backend

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import MainLayout from '../layouts/MainLayout'
+import axios from 'axios'
 
 function POSPage() {
+    //state to hold products
+    const [products, setProducts] = useState([])
+
+
+    //fetch products from backend api
+    const fetchProducts = async () => {
+        const result = await axios.get('products')
+        setProducts(await result.data)
+    }
+    
+    //fetch products on component mount
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+
     return (
-        <div>POSPage</div>
+        <div>
+            <MainLayout>
+                POS Content
+            </MainLayout>
+
+        </div>
     )
 }
 
