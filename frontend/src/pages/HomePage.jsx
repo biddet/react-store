3210833c7b0db972e26eafcd9c8df99e84f74968
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-03-23 16:08:26 +1100

    add navbar to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+        <header>
+            <nav className="navbar bg-primary">
+                <div className="container">
+                    <Link to="/" className="navbar-brand" >DevPOS</Link>
+                </div>
+            </nav>
+
+        </header>
+        <main>
+
+        </main>
+    </div>
   )
 }
 
