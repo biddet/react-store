251400b16eec58d3ed280a1f86292e137c8a1dd0
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-03-23 16:19:33 +1100

    add content to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,10 +10,18 @@ function HomePage() {
                     <Link to="/" className="navbar-brand" >DevPOS</Link>
                 </div>
             </nav>
-
         </header>
         <main>
+            <div className="container mt-3">
+              <div className="bg-light p-5 mt-4 rounded-3">
+                    <h1>Welcome to the simple POS for small businesses</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur numquam minima quis ex voluptatem doloremque excepturi nostrum omnis quos sapiente, nihil unde laborum quidem similique sint quam dicta aut fugiat.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt numquam explicabo accusamus consequuntur praesentium qui maxime, ad neque sint, sed rem doloremque aliquam itaque odit minima, aspernatur ex quidem accusantium?</p>
+                    <Link to="/pos" className="btn btn-primary"> Click here to sell products</Link>
+              </div>
 
+
+            </div>
         </main>
     </div>
   )
--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function POSPage() {
-  return (
-    <div>POSPage</div>
-  )
+    return (
+        <div>POSPage</div>
+    )
 }
 
 export default POSPage
