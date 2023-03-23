ac8e36d7889d68c314567a3f4f8ab8a46353bf2a
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-03-23 16:59:27 +1100

    render retrieved data

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -5,12 +5,16 @@ import axios from 'axios'
 function POSPage() {
     //state to hold products
     const [products, setProducts] = useState([])
+    //state to hold loading status
+    const [isLoading, setIsLoading] = useState(false)
 
 
     //fetch products from backend api
     const fetchProducts = async () => {
+        setIsLoading(true)
         const result = await axios.get('products')
         setProducts(await result.data)
+        setIsLoading(false)
     }
     
     //fetch products on component mount
@@ -22,9 +26,26 @@ function POSPage() {
     return (
         <div>
             <MainLayout>
-                POS Content
-            </MainLayout>
+                <div className="row">
+                    <div className="col-lg-8">
+                        {isLoading ? "Loading..." : <div className="row">
+                            {products.map((product, key) => 
+                                <div key={key} className= "col-lg-4">
+                                    <div className="border">
+                                        <p>{product.name}</p>
+                                        <img src={product.image} alt={product.name} className="img-fluid" />
+                                        <p>${product.price}</p>
+                                    </div>
+                                </div>
+                            )}  
+                            </div>
+                        }                            
+                    </div>
+                    <div className="col-lg-4">
 
+                    </div>
+                </div>
+            </MainLayout>
         </div>
     )
 }
